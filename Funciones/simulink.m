61579db1a026c50404c396021a499d409fef3aeb
Estela Mora <estela.mora.barba@alumnos.upm.es>
2024-12-22 17:35:26 +0100

    Perturbación simulink

--- Funciones/simulink.m
+++ Funciones/simulink.m
@@ -11,11 +11,17 @@ tiempo = simulacion.out(:,1);
 salida = simulacion.out(:,2);
 
 t_ini = tiempo(find(salida ~= 0, 1));
-t_fin = tiempo(end);
-x_ini = find(tiempo==t_ini); 
-x_fin = find(tiempo==t_fin); 
-tiempo_interes = tiempo(x_ini:x_fin)-tiempo(x_ini); 
-salida_interes = salida(x_ini:x_fin); 
+
+if(t_ini~=[])
+    t_fin = tiempo(end);
+    x_ini = find(tiempo==t_ini); 
+    x_fin = find(tiempo==t_fin); 
+    tiempo_interes = tiempo(x_ini:x_fin)-tiempo(x_ini); 
+    salida_interes = salida(x_ini:x_fin); 
+else
+    tiempo_interes = tiempo;
+    salida_interes = salida;
+end
 
 limite = find(tiempo_interes <= 5);
 tiempo_interes = tiempo_interes(limite);
